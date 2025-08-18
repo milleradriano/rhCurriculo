618c2fce49e30c939474398ced66f0f196e14c46
milleradriano <milleradriano@gmail.com>
2025-08-18 17:23:48 -0300

    Ajuste da busca do cep e dados da residencia

--- FRONTEND-CLIENTE/src/app/service/residencia.service.ts
+++ FRONTEND-CLIENTE/src/app/service/residencia.service.ts
@@ -1,65 +1,64 @@
 import { Inject, Injectable, OnInit, PLATFORM_ID } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
-import { Observable, throwError,catchError,shareReplay, take } from 'rxjs';
-import { interfaceResidencia,interfaceCep} from '../interface/residencia';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from '@angular/common/http';
+import { Observable, throwError, catchError, shareReplay, take } from 'rxjs';
+import { interfaceResidencia, interfaceCep } from '../interface/residencia';
 import { environment } from '../../environments/environment.prod';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
-export class ResidenciaService  {
-  private url = environment.api
-  sessionToken: string | null = sessionStorage.getItem('token'); 
-  cpf: any  = sessionStorage.getItem('cpf');
+export class ResidenciaService {
+  private url = environment.api;
+  sessionToken: string | null = sessionStorage.getItem('token');
+  cpf: any = sessionStorage.getItem('cpf');
   idcandidato: any = sessionStorage.getItem('codcand');
-  constructor(private httpclient: HttpClient,@Inject(PLATFORM_ID) private platformId: Object) { }
+  constructor(
+    private httpclient: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
-getCep(cep: any, headers?: any): Observable<interfaceCep> {
-  console.log('cep no get',cep)
-  const params = new HttpParams().set('cep', cep);
-  return this.httpclient
-    .get<interfaceCep>(this.url+'/cep',{params,   headers })
-    .pipe(take(1), catchError(this.handleError));    
-}
-
-/*************  ✨ Windsurf Command 🌟  *************/
-getResidencia(valor:any, headers?: any): Observable<interfaceResidencia[]> {
-  console.log('residencia no servi', valor.idcandidato, valor.cpf);
-  const valores = { valor};
-  const params = new HttpParams()
-    .set('idcandidato', valor.idcandidato || this.idcandidato)
-    .set('cpf', valor.cpf || this.cpf);
-// ESSE GET ESTA COMO POST  .post<interfaceResidencia[]> PQ ESTOU ENVIANDO CPF NO BODY
-// CASO NAO FOSSE NO BOBY SERIA POR PARAMETRO E USARIA GET
-  return this.httpclient
-    .post<interfaceResidencia[]>(this.url + '/retornaresidencia', valor, {headers })
-    .pipe(take(1), catchError(this.handleError));
-}
-
-
-
-postResidencia(valores: any,headers?: any): Observable<interfaceResidencia> {
-  console.log('cpf service',typeof(valores)) 
-  return this.httpclient
-    .post<interfaceResidencia>(this.url+'/residencia',  valores, { headers })
-    .pipe(take(1), catchError(this.handleError));
-
-}  
-
-private handleError(error: HttpErrorResponse) {
-  let errorMessage = 'Ocorreu um erro desconhecido';
-  
-  if (error.error instanceof ErrorEvent) {
-    // Erro do cliente
-    errorMessage = `Erro: ${error.error.message}`;
-  } else {
-    // Erro do servidor
-    errorMessage = `Código do erro: ${error.status}, ` + 
-                  `mensagem: ${error.message}`;
+  getCep(cep: any, headers?: any): Observable<interfaceCep[]> {
+    console.log('cep no get', cep);
+    const params = new HttpParams().set('cep', cep);
+    return this.httpclient
+      .get<interfaceCep[]>(`${this.url}/cep/${cep}`, { params, headers })
+      .pipe(take(1), catchError(this.handleError));
   }
-  
-  console.error(errorMessage);
-  return throwError(() => new Error(errorMessage));
-}
+
+  /*************  ✨ Windsurf Command 🌟  *************/
+  getResidencia(valor: any, headers?: any): Observable<interfaceResidencia[]> {
+    console.log('residencia no servi', valor.idcandidato, valor.cpf);
+    const valores = new HttpParams().set('idcandidato', valor.idcandidato || this.idcandidato)
+      .set('cpf', valor.cpf || this.cpf);
+    return this.httpclient
+      .get<interfaceResidencia[]>(this.url + '/residencia', { params: valores, headers })
+      .pipe(take(1), catchError(this.handleError));
+  }
+
+  postResidencia(valores: any, headers?: any): Observable<interfaceResidencia> {
+    console.log('cpf service', typeof valores);
+    return this.httpclient
+      .post<interfaceResidencia>(this.url + '/residencia', valores, { headers })
+      .pipe(take(1), catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Ocorreu um erro desconhecido';
+
+    if (error.error instanceof ErrorEvent) {
+      // Erro do cliente
+      errorMessage = `Erro: ${error.error.message}`;
+    } else {
+      // Erro do servidor
+      errorMessage =
+        `Código do erro: ${error.status}, ` + `mensagem: ${error.message}`;
+    }
+
+    console.error(errorMessage);
+    return throwError(() => new Error(errorMessage));
+  }
 }
